b51b5ec2b3b56d204a541e1f67430d021866f2c9
Al Haji-Ali <a.hajiali@hw.ac.uk>
2022-07-15 23:38:29 +0100

    Replace `if` with `when` and avoid long lines.

--- procress.el
+++ procress.el
@@ -25,7 +25,7 @@
 ;; This package shows a progress indicator for a process in the modeline.
 ;; Typical usage:
 ;;
-;; (require 'process)
+;; (require 'procress)
 ;; (procress-load-default-svg-images)
 ;; (add-hook 'LaTeX-mode-hook 'tex-procress-mode)
 ;;
@@ -122,17 +122,31 @@ SUCCESS is non-nil if the process is successful."
 
 ;;;;;;;;;;;;;;;;;;;;;;;; SVG specific functions and constants
 (defconst procress--svg-wrench
-  '((width . 24) (height . 24) (path . "M13,2.03V2.05L13,4.05C17.39,4.59 20.5,8.58 19.96,12.97C19.5,16.61 16.64,19.5 13,19.93V21.93C18.5,21.38 22.5,16.5 21.95,11C21.5,6.25 17.73,2.5 13,2.03M11,2.06C9.05,2.25 7.19,3 5.67,4.26L7.1,5.74C8.22,4.84 9.57,4.26 11,4.06V2.06M4.26,5.67C3,7.19 2.25,9.04 2.05,11H4.05C4.24,9.58 4.8,8.23 5.69,7.1L4.26,5.67M2.06,13C2.26,14.96 3.03,16.81 4.27,18.33L5.69,16.9C4.81,15.77 4.24,14.42 4.06,13H2.06M7.1,18.37L5.67,19.74C7.18,21 9.04,21.79 11,22V20C9.58,19.82 8.23,19.25 7.1,18.37M16.82,15.19L12.71,11.08C13.12,10.04 12.89,8.82 12.03,7.97C11.13,7.06 9.78,6.88 8.69,7.38L10.63,9.32L9.28,10.68L7.29,8.73C6.75,9.82 7,11.17 7.88,12.08C8.74,12.94 9.96,13.16 11,12.76L15.11,16.86C15.29,17.05 15.56,17.05 15.74,16.86L16.78,15.83C17,15.65 17,15.33 16.82,15.19Z"))
+  '((width . 24) (height . 24)
+    (path . "M13,2.03V2.05L13,4.05C17.39,4.59 20.5,8.58 19.96,12.97C19.5,16.61\
+ 16.64,19.5 13,19.93V21.93C18.5,21.38 22.5,16.5 21.95,11C21.5,6.25 17.73,2.5 1\
+3,2.03M11,2.06C9.05,2.25 7.19,3 5.67,4.26L7.1,5.74C8.22,4.84 9.57,4.26 11,4.06\
+V2.06M4.26,5.67C3,7.19 2.25,9.04 2.05,11H4.05C4.24,9.58 4.8,8.23 5.69,7.1L4.26\
+,5.67M2.06,13C2.26,14.96 3.03,16.81 4.27,18.33L5.69,16.9C4.81,15.77 4.24,14.42\
+ 4.06,13H2.06M7.1,18.37L5.67,19.74C7.18,21 9.04,21.79 11,22V20C9.58,19.82 8.23\
+,19.25 7.1,18.37M16.82,15.19L12.71,11.08C13.12,10.04 12.89,8.82 12.03,7.97C11.\
+13,7.06 9.78,6.88 8.69,7.38L10.63,9.32L9.28,10.68L7.29,8.73C6.75,9.82 7,11.17 \
+7.88,12.08C8.74,12.94 9.96,13.16 11,12.76L15.11,16.86C15.29,17.05 15.56,17.05 \
+15.74,16.86L16.78,15.83C17,15.65 17,15.33 16.82,15.19Z"))
   "SVG related data that represents a wrench.
 Taken from `Templarian/MaterialDesign', to be used with `procress--svg-create'")
 
 (defconst procress--svg-alert
-  '((width . 24) (height . 24) (path . "M13 14H11V9H13M13 18H11V16H13M1 21H23L12 2L1 21Z"))
+  '((width . 24) (height . 24)
+    (path . "M13 14H11V9H13M13 18H11V16H13M1 21H23L12 2L1 21Z"))
   "SVG related data that represents an alert.
 Taken from `Templarian/MaterialDesign', to be used with `procress--svg-create'")
 
 (defconst procress--svg-wrench-check
-  '((width . 24) (height . 24) (path . "M9 2C10.8 2.6 12 4.3 12 6.2C12 8.2 10.8 9.9 9 10.5V21.5C9 21.8 8.8 22 8.5 22H6.5C6.2 22 6 21.8 6 21.4V10.4C4.2 9.8 3 8.1 3 6.2S4.2 2.6 6 2V5.7H9V2M20.6 13L22 14.41L15.47 21L12 17.5L13.4 16.09L15.47 18.17L20.6 13"))
+  '((width . 24) (height . 24)
+    (path . "M9 2C10.8 2.6 12 4.3 12 6.2C12 8.2 10.8 9.9 9 10.5V21.5C9 21.8 8.\
+8 22 8.5 22H6.5C6.2 22 6 21.8 6 21.4V10.4C4.2 9.8 3 8.1 3 6.2S4.2 2.6 6 2V5.7H\
+9V2M20.6 13L22 14.41L15.47 21L12 17.5L13.4 16.09L15.47 18.17L20.6 13"))
   "SVG related data that represents a wrench with a check.
 Taken from `Templarian/MaterialDesign', to be used with `procress--svg-create'")
 
@@ -167,7 +181,7 @@ the center. "
     (when rotate
       (let ((transform (plist-get args :transform))
             (rotate-pos (cl-position :rotate args)))
-        (if transform
+        (when transform
             ;; Remove rotate and value completelyn
             (setq args
                   (cl-delete-if (lambda (_) t)
@@ -223,9 +237,9 @@ the center. "
 (defun procress--tex-done (process _)
   "Update modeline to indicate termination of a tex process.
 Effective only when `tex-procress-mode' is `t'."
-  (if tex-procress-mode
-      (with-current-buffer (procress--tex-command-buffer process)
-        (procress-done (not (TeX-error-report-has-errors-p))))))
+  (when tex-procress-mode
+    (with-current-buffer (procress--tex-command-buffer process)
+      (procress-done (not (TeX-error-report-has-errors-p))))))
 
 (defun procress--tex-command-buffer (process &rest _)
   "Returns modeline buffer for tex processes"
@@ -239,8 +253,8 @@ Effective only when `tex-procress-mode' is `t'."
 (defun procress--tex-progress (process &rest _)
   "Update modeline to indicate progress of a tex process.
 Effective only when `tex-procress-mode' is `t'."
-  (if tex-procress-mode
-      (with-current-buffer (procress--tex-command-buffer process)
-        (procress-progress))))
+  (when tex-procress-mode
+    (with-current-buffer (procress--tex-command-buffer process)
+      (procress-progress))))
 
 (provide 'procress)
